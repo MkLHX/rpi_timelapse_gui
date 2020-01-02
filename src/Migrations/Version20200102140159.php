5a43b6930fb05bfaf42df77fc94ff62da91dc829
MkLHX <mickael.lehoux@gmail.com>
2020-01-02 15:02:13 +0100

    fix migrations

--- src/Migrations/Version20200102140159.php
+++ src/Migrations/Version20200102140159.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191212113313 extends AbstractMigration
+final class Version20200102140159 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20191212113313 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('ALTER TABLE timelapse ADD COLUMN file_extension VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE ftptransfert (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, host VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL, active BOOLEAN NOT NULL)');
+        $this->addSql('CREATE TABLE timelapse (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, resolution VARCHAR(255) NOT NULL, file_extension VARCHAR(255) NOT NULL, schedule VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL)');
     }
 
     public function down(Schema $schema) : void
@@ -30,10 +31,7 @@ final class Version20191212113313 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('CREATE TEMPORARY TABLE __temp__timelapse AS SELECT id, resolution, schedule, path FROM timelapse');
+        $this->addSql('DROP TABLE ftptransfert');
         $this->addSql('DROP TABLE timelapse');
-        $this->addSql('CREATE TABLE timelapse (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, resolution VARCHAR(255) , schedule DATETIME , path VARCHAR(255) )');
-        $this->addSql('INSERT INTO timelapse (id, resolution, schedule, path) SELECT id, resolution, schedule, path FROM __temp__timelapse');
-        $this->addSql('DROP TABLE __temp__timelapse');
     }
 }
