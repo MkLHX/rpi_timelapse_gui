ec4c74e5f370f53808ba61c3bfc407d1ed98d2d6
MkLHX <mickael.lehoux@gmail.com>
2020-01-06 16:25:59 +0100

    debug

--- src/Controller/TimelapseController.php
+++ src/Controller/TimelapseController.php
@@ -94,7 +94,7 @@ class TimelapseController extends AbstractController
         return $this->render('timelapse/timelapse.html.twig', [
             'timelapseForm' => $timelapseForm->createView(),
             'ftpForm' => $ftpForm->createView(),
-            'pictures' => krsort($pictures),
+            'pictures' => arsort($pictures),
         ]);
     }
 
